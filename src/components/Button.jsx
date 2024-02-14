5892ad94bc36d6bed13cb3fea2929fbba2986327
Anubhav <lucimgstar007@gmail.com>
2024-02-14 13:21:43 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ category, applyFilter }) => {
   return (
     <button
       onClick={() => applyFilter(category)}
-      className="bg-[#ffffff] h-8 w-14 ml-4 rounde-lg text-black  rounded-md"
+      className="bg-[#ffffff] h-8 w-14 ml-4 rounde-lg text-black  rounded-md hover:bg-[#86B6F6]"
     >
       {category}
     </button>
