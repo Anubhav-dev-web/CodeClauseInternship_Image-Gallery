24b925cd4e4fbbc14f1d04fe0a45898237a6d543
Anubhav <lucimgstar007@gmail.com>
2024-02-14 13:22:47 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -21,25 +21,23 @@ const Gallery = ({ data }) => {
       : data.filter((item) => item.category === selectedCategory);
 
   return (
-    <div className="container mx-auto mt-8 ">
-      <div className="mb-10">
-        <label className="mr-2 text-2xl font-bold text-white ">Filters:</label>
-        <select
-          className="border p-2 rounded-xl"
-          value={selectedCategory}
-          onChange={(e) => handleCategoryChange(e.target.value)}
-        >
-          {uniqueCategories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  ">
-        {filteredData.map((item) => (
-          <Card key={item.id} {...item} />
+   <div className="container mx-auto mt-8 ">
+      <div className=" ml-14">
+        <label className="text-2xl font-bold text-white ">Filters:</label>
+        {uniqueCategories.map((category) => (
+          <Button
+            key={category}
+            category={category}
+            applyFilter={() => handleCategoryChange(category)}
+          />
         ))}
+      </div>
+      <div className=" p-10">
+        <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  ">
+          {filteredData.map((item) => (
+            <Card key={item.id} {...item} />
+          ))}
+        </div>
       </div>
     </div>
   );
